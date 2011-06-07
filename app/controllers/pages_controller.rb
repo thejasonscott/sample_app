ad8cad86f3cdc9de6129284a505e3af03f60f05a
Jason Scott <jason@bundl.com.au>
2011-06-07 12:35:31 +1000

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def Home
+    @title = "Home"
   end
 
   def Contact
+    @title = "Contact"
   end
   
   def about
+  @title = "About"
   end
 
 end
